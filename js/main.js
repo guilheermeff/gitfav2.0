55117d172f99f3a0c06f4d36d92b66251369a2f4
Guilherme Fernandes <guilheermeff@outlook.com>
2022-08-19 18:19:14 -0400

    create createRow()

--- js/main.js
+++ js/main.js
@@ -3,15 +3,49 @@ class Favorites {
   constructor(root) {
     this.page = document.querySelector(root)
   }
+
+  load() {
+    this.entries = [
+      {
+        login: 'guilheermeff',
+        name: 'Guilherme Fernandes',
+        public_repos: 30,
+        followers: 50
+      },{}]
+  }
 }
 
 class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
+    this.tbody = this.page.querySelector('table tbody')
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td class="user">
+      <img src="https://github.com/guilheermeff.png" alt="Imagem de guilheermeff">
+      <a href="https://github.com/guilheermeff" target="_blank">
+        <p>Guilherme Fernandes</p>
+        <span>/guilheermeff</span>
+      </a>
+    </td>
+    <td class="repositories">
+      76
+    </td>
+    <td class="followers">
+      9589
+    </td>
+    <td class="action">
+      <button class="remove">&times;</button>
+    </td>  
+    `
+    return tr
   }
 
   removeAllTr() {
-    this.tbody = this.page.querySelector('table tbody')
     this.tbody.querySelectorAll('tr').forEach(tr => {tr.remove()})
   }
 }
